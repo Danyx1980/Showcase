8efd50a9e2f7e5b0487285ac21ab193f0776faf1
Daniel <daniel.chan.valette@gmail.com>
2016-09-06 16:52:27 +0000

    upload

---
+++ public/scripts/upload.js
@@ -0,0 +1,45 @@
+(() => {
+    document.getElementById("fileUpload").onchange = () => {
+        const files = document.getElementById("fileUpload").files;
+        const file = files[0];
+        
+        if(file === null){
+            return alert ("no file selected.");
+        }
+        getSignedRequest(file);
+    }
+})();
+
+function getSignedRequest(file){
+    cont xhr = new XMLHttpRequest();
+    xhr.open("GET", `/sign-s3?file-name=${file.name}&file-type=${file.type}`);
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                const response = JSON.parse(xhr.responseText);
+                uploadFile(file, response.signedRequest, response.url);
+            }
+            else{
+                alert("could not get signed URL.");
+            }
+        }
+    };
+    xhr.send();
+}
+
+function uploadFile(file, signedRequest, url){
+    const xhr = new XMLHttpRequest();
+    xhr.open("PUT", signedRequest);
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState === 4){
+            if(xhr.status === 2000){
+                document.getElementById("fileLink").value = url;
+                console.log("File uploaded to " + url);
+            }
+        }
+        else {
+            console.log("Couldn't upload file");
+        }
+    };
+    xhr.send(file);
+}
