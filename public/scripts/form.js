2ba9c1afbe1c76d42cf5a403f6d7f28cb26026e0
Daniel <daniel.chan.valette@gmail.com>
2016-09-12 20:53:15 +0000

    fixed comment edit

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -1,12 +1,67 @@
-document.getElementById("addCommentForm").style.display = "none";
+InitializeCommentForm();
+InitializePictureEditForm();
+InitializeCommentEditForm();
 
-document.getElementById("toggleComment").addEventListener("click", function(){
-   var form = document.getElementById("addCommentForm");
+function InitializeCommentForm(){
+
+   var addCommentButton = document.querySelector("#toggleComment");
    
-   if(form.style.display === "none"){
-       form.style.display = "block";
-       window.scrollTo(0,document.body.scrollHeight);
-   } else {
-       form.style.display = "none";
+   if(addCommentButton){
+      var form = document.getElementById("addCommentForm");
+      form.style.display = "none";
+      addCommentButton.addEventListener("click", function(){
+         if(form.style.display === "none"){
+             form.style.display = "block";
+             window.scrollTo(0,document.body.scrollHeight);
+         } else {
+             form.style.display = "none";
+         }   
+      });
    }
-});
+}
+
+ function InitializeCommentEditForm(){
+
+   var editCommentButton = document.querySelectorAll(".toggleEditComment");
+   
+   if(editCommentButton.length >= 1){
+      
+      var editCommentForms = document.querySelectorAll(".editCommentForm");
+      
+      for(var i = 0; i < editCommentButton.length; i++){
+         
+         editCommentForms[i].style.display = "none";
+         console.log(editCommentForms[i].style.display);
+         editCommentButton[i].addEventListener("click", showEditCommentForm(editCommentForms[i]));
+      }
+   }
+}
+
+function showEditCommentForm(commentForm){
+   return function(){
+      if(commentForm.style.display === "none"){
+         commentForm.style.display = "block";
+      } else {
+         commentForm.style.display = "none";
+      }
+   }
+}
+
+function InitializePictureEditForm(){
+
+   var editPictureButton = document.querySelector("#editPictureToggle");
+   
+   if(editPictureButton){
+      var form = document.querySelector("#editPictureForm");
+      
+      form.style.display = "none";
+      
+      editPictureButton.addEventListener("click", function(){
+         if(form.style.display === "none"){
+            form.style.display = "block";
+         } else {
+            form.style.display = "none";
+         }
+      });
+   }
+}
